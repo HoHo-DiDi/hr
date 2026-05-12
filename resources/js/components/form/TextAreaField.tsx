778948eb0b6642172930cc3327bb9b0b438de594
zawsainan <zawsainan@gmail.com>
2026-05-12 22:32:13 +0630

    Added some components

---
+++ resources/js/components/form/TextAreaField.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { forwardRef, TextareaHTMLAttributes, useId } from "react";
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+
+interface TextAreaFieldProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    label?: string;
+    description?: string;
+    error?: string;
+}
+const TextAreaField = forwardRef<HTMLTextAreaElement, TextAreaFieldProps>(({ id, className, error, label, description, ...props }, ref) => {
+    const inputId = id || useId();
+    return (<div className={cn("w-full grid gap-2 items-center", className)}>
+        {label && <Label htmlFor={inputId}>{label}</Label>}
+        <Textarea id={inputId} className={cn(error && "border-red-500 focus-visible:ring-red-500")} ref={ref} {...props}></Textarea>
+        {!error && description && <p className="text-sm text-muted-foreground">{description}</p>}
+        {error && <p className="text-sm text-red-500 font-medium">{description}</p>}
+    </div>)
+});
+TextAreaField.displayName = "TextAreaField"
---
+++ resources/js/components/form/TextField.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { forwardRef, InputHTMLAttributes, useId } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    error?: string;
+}
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(({ label, error, id, className, ...props }, ref) => {
+    const inputId = id || useId();
+    return (
+        <div className={cn('w-full grid items-center gap-2', className)}>
+            {label && <Label htmlFor={inputId} >{label}</Label>}
+            <Input id={inputId} className={cn(error && "border-red-500 focus-visible:ring-red-500")} {...props} ref={ref} />
+            {error && <p className="text-sm text-red-500 font-medium">{error}</p>}
+        </div>
+    )
+});
+TextField.displayName = "TextField";
