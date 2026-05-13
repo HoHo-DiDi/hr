53f1b43105d8ae5fe5d0fc8ada762932caa2b173
Htet Aung Lwin <dev.htetaunglwin@gmail.com>
2026-05-13 21:44:45 +0630

    migrations

---
+++ database/migrations/2026_05_13_140307_create_finger_prints_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\AttendanceStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('finger_prints', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->cascadeOnDelete();
+            $table->enum('attendance', AttendanceStatus::cases());
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('finger_prints');
+    }
+};
